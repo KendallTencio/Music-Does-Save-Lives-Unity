c59c8420763ce2a6ffc1b8d2ae79bed3ae9489b6
Jorge Gutiérrez <jorgeedugutima15@gmail.com>
2020-10-17 21:04:34 -0600

    Mecánica de disparo básica

---
+++ MusicDoesSaveLives/Assets/Scripts/ShootBehavoir.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBehavoir : MonoBehaviour
+{
+    public float lifeTime = 3.0f;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start(){
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //s = v * t        
+        this.transform.Translate(Vector3.up * speed * Time.deltaTime); //vector3.up es hacia el frente del objeto
+
+    }
+   
+}
---
+++ MusicDoesSaveLives/Assets/Scripts/TileBehavoir.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehavoir : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public Rigidbody bullet;
+    //Disparo
+    public Transform bullet;
+    public float laserDistance = 0.2f;
+    public float timeBetweenFires = 0.3f;
+    public float timeUntilNextFire = 0.0f;
+    //public List<KeyCode> shootButton;
+    public List<KeyCode> shootButton;
+    //Funcionas básicas de Unity    
+    void Update()
+    {
+        Fire(); 
+    }
+    //Funciones propias
+    void Fire(){      
+        foreach(KeyCode key in shootButton){
+            if(Input.GetKey(key) && timeUntilNextFire < 0) {
+            //disparo
+            timeUntilNextFire = timeBetweenFires;
+            ShootLaser();   
+            break;  
+            }    
+        }
+        timeUntilNextFire -= Time.deltaTime;
+    }
+    void ShootLaser(){        
+        Vector3 laserPos = this.transform.position; //la pos de la tale        
+        float rotationAngle = this.transform.localEulerAngles.z - 90;        
+        laserPos.x += Mathf.Cos(rotationAngle * Mathf.Deg2Rad) * laserDistance;
+        laserPos.y += Mathf.Sin(rotationAngle * Mathf.Deg2Rad) * laserDistance;
+        Instantiate(bullet,laserPos,this.transform.rotation);
+
+    }    
+}
