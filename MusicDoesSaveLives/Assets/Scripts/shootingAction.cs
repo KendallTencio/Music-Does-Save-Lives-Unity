76fb2496345f5522f00e43a000d44b34b1b382a4
Kendall Tencio Esquivel <kendalltencio132e@gmail.com>
2020-10-17 18:03:47 -0600

    TilesColorcitos

---
+++ MusicDoesSaveLives/Assets/Scripts/shootingAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootingAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float bulletSpeed = 10;
+    public Rigidbody bullet;
+
+    void Fire()
+    {
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = transform.forward * bulletSpeed;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.L))
+        {
+            Fire();
+        }
+            
+    }
+}
---
+++ MusicDoesSaveLives/Assets/Scripts/tileAction.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileAction : MonoBehaviour
+{
+    public GameObject colorCube;
+    public GameObject cTile;
+    public GameObject dTile;
+    public GameObject eTile;
+    public GameObject fTile;
+    public GameObject gTile;
+    public GameObject aTile;
+    public GameObject bTile;
+    // Use this for initialization
+    void Start()
+    {
+        cTile.GetComponent<Renderer>().material.color = Color.white;
+        dTile.GetComponent<Renderer>().material.color = Color.white;
+        eTile.GetComponent<Renderer>().material.color = Color.white;
+        fTile.GetComponent<Renderer>().material.color = Color.white;
+        gTile.GetComponent<Renderer>().material.color = Color.white;
+        aTile.GetComponent<Renderer>().material.color = Color.white;
+        bTile.GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            cTile.GetComponent<Renderer>().material.color = Color.gray;
+            StartCoroutine(cleanTile());
+            colorCube.GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            dTile.GetComponent<Renderer>().material.color = Color.gray;
+            StartCoroutine(cleanTile());
+            colorCube.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            eTile.GetComponent<Renderer>().material.color = Color.gray;
+            StartCoroutine(cleanTile());
+            colorCube.GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            fTile.GetComponent<Renderer>().material.color = Color.gray;
+            StartCoroutine(cleanTile());
+            colorCube.GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            gTile.GetComponent<Renderer>().material.color = Color.gray;
+            StartCoroutine(cleanTile());
+            colorCube.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            aTile.GetComponent<Renderer>().material.color = Color.gray;
+            StartCoroutine(cleanTile());
+            colorCube.GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            bTile.GetComponent<Renderer>().material.color = Color.gray;
+            StartCoroutine(cleanTile());
+            colorCube.GetComponent<Renderer>().material.color = Color.cyan;
+        }
+    }
+
+    
+
+
+    private IEnumerator cleanTile()
+    {
+        yield return new WaitForSeconds(0.5f);
+        cTile.GetComponent<Renderer>().material.color = Color.white;
+        dTile.GetComponent<Renderer>().material.color = Color.white;
+        eTile.GetComponent<Renderer>().material.color = Color.white;
+        fTile.GetComponent<Renderer>().material.color = Color.white;
+        gTile.GetComponent<Renderer>().material.color = Color.white;
+        aTile.GetComponent<Renderer>().material.color = Color.white;
+        bTile.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
