e3ee9bf058fe0067a6a4e4a4b532c1ae32d43dcc
Jorge Gutiérrez <jorgeedugutima15@gmail.com>
2020-10-17 21:59:00 -0600

    Notitas con sonidos Sci-Fi
    Cada una ligada a su tecla correspondiente

--- MusicDoesSaveLives/Assets/Scripts/TileBehavoir.cs
+++ MusicDoesSaveLives/Assets/Scripts/TileBehavoir.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+
 
 public class TileBehavoir : MonoBehaviour
 {
@@ -11,9 +13,14 @@ public class TileBehavoir : MonoBehaviour
     public float laserDistance = 0.2f;
     public float timeBetweenFires = 0.3f;
     public float timeUntilNextFire = 0.0f;
+    private AudioSource audioSource;
     //public List<KeyCode> shootButton;
     public List<KeyCode> shootButton;
     //Funcionas básicas de Unity    
+    void  Start()
+    {
+        audioSource= GetComponent<AudioSource>();
+    }
     void Update()
     {
         Fire(); 
@@ -31,6 +38,7 @@ public class TileBehavoir : MonoBehaviour
         timeUntilNextFire -= Time.deltaTime;
     }
     void ShootLaser(){        
+        audioSource.Play();
         Vector3 laserPos = this.transform.position; //la pos de la tale        
         float rotationAngle = this.transform.localEulerAngles.z - 90;        
         laserPos.x += Mathf.Cos(rotationAngle * Mathf.Deg2Rad) * laserDistance;
