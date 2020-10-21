45503abfd7b7edf4390ccf0eca4d54910f2287db
Kendall Tencio Esquivel <kendalltencio132e@gmail.com>
2020-10-20 22:05:04 -0600

    DetectionTiles

---
+++ MusicDoesSaveLives/Assets/Scripts/FallingObjectBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjectBehavior : MonoBehaviour
+{
+
+    public bool canBePressed;
+    public KeyCode keyPressed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(keyPressed))
+        {
+            if (canBePressed)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Detector")
+        {
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        if (other.tag == "Detector")
+        {
+            canBePressed = false;
+        }
+    }
+}
