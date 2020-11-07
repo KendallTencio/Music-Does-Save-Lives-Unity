0828605b93e355cd14dc87bcf2607dcaf280ca8a
Sebastian Segura <sebassegurasoto@gmail.com>
2020-11-07 00:15:17 -0600

    Title Screen
    Se adjunta la pantalla de inicio y unas transiciones hechas hasta la Level Select Screen.

---
+++ MusicDoesSaveLives/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 2f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            LoadNextLevel();
+        }
+    }
+    public void LoadNextLevel()
+    {
+        StartCoroutine(loadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator loadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
