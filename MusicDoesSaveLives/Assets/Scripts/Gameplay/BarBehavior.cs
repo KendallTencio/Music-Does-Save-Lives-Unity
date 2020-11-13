59ffc44d81d50e2feebe0f7e2729cf697fcaaa54
Kendall Tencio Esquivel <kendalltencio132e@gmail.com>
2020-11-13 15:09:23 -0600

    Update: Fixed "Error Notes"

--- MusicDoesSaveLives/Assets/Scripts/Gameplay/BarBehavior.cs
+++ MusicDoesSaveLives/Assets/Scripts/Gameplay/BarBehavior.cs
@@ -7,49 +7,46 @@ public class BarBehavior : MonoBehaviour
     public bool canBePressed;
     public KeyCode[] keyPressed;
     public AudioSource damageSound;
+    public int objectsEntered = 0;
 
     //CollissionPlatformBehavior for Damage
     public CollissionPlatformBehavior colPlatf;
 
-    // Update is called once per frame
     void Update()
-    {
+    {   
         for (int i = 0; i < keyPressed.Length; i++)
         {
             if (Input.GetKeyDown(keyPressed[i]))
             {
                 if (canBePressed)
                 {
-                    StartCoroutine(canBePressedOff()); 
+                    if (objectsEntered == 1)
+                    {
+                        canBePressed = false;
+                    }
+                    objectsEntered--;
                 }
                 else
                 {
-                    Debug.Log("Damage with: " + this);
                     damageSound.Play();
                     colPlatf.realDamage--;
                 }
             }
         }
-
     }
 
-    private IEnumerator canBePressedOff()
+    private void OnTriggerEnter(Collider other)
     {
-        yield return new WaitForSeconds(0.35f);
-        canBePressed = false;
-    }
-
-        private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "FObject")
+        if (other.gameObject.CompareTag("FObject"))
         {
+            objectsEntered++;
             canBePressed = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "FObject")
+        if (other.gameObject.CompareTag("FObject"))
         {
             canBePressed = false;
         }
